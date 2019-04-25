d5b2189a1bc56e1aa24ec02a7deb824d7f475077
Isylda <noemie.lecleve@live.fr>
2019-04-25 11:34:19 +0200

    BO - Création des objets

--- src/fr/eni/javaee/ebay/bo/ArticleVendu.java
+++ src/fr/eni/javaee/ebay/bo/ArticleVendu.java
@@ -1,9 +1,162 @@
 package fr.eni.javaee.ebay.bo;
 
+import java.util.Date;
+
 public class ArticleVendu {
+
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private int etatVente;
 
 	public ArticleVendu() {
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param noArticle
+	 * @param nomArticle
+	 * @param description
+	 * @param dateDebutEncheres
+	 * @param dateFinEncheres
+	 * @param miseAPrix
+	 * @param prixVente
+	 * @param etatVente
+	 */
+	public ArticleVendu(int noArticle, String nomArticle, String description, Date dateDebutEncheres,
+			Date dateFinEncheres, int miseAPrix, int prixVente, int etatVente) {
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+	/**
+	 * @return the noArticle
+	 */
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	/**
+	 * @param noArticle
+	 *            the noArticle to set
+	 */
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	/**
+	 * @return the nomArticle
+	 */
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	/**
+	 * @param nomArticle
+	 *            the nomArticle to set
+	 */
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the dateDebutEncheres
+	 */
+	public Date getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	/**
+	 * @param dateDebutEncheres
+	 *            the dateDebutEncheres to set
+	 */
+	public void setDateDebutEncheres(Date dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	/**
+	 * @return the dateFinEncheres
+	 */
+	public Date getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	/**
+	 * @param dateFinEncheres
+	 *            the dateFinEncheres to set
+	 */
+	public void setDateFinEncheres(Date dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	/**
+	 * @return the miseAPrix
+	 */
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	/**
+	 * @param miseAPrix
+	 *            the miseAPrix to set
+	 */
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	/**
+	 * @return the prixVente
+	 */
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+	/**
+	 * @param prixVente
+	 *            the prixVente to set
+	 */
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	/**
+	 * @return the etatVente
+	 */
+	public int getEtatVente() {
+		return etatVente;
+	}
+
+	/**
+	 * @param etatVente
+	 *            the etatVente to set
+	 */
+	public void setEtatVente(int etatVente) {
+		this.etatVente = etatVente;
 	}
 
 }
--- src/fr/eni/javaee/ebay/bo/Enchere.java
+++ src/fr/eni/javaee/ebay/bo/Enchere.java
@@ -1,9 +1,98 @@
 package fr.eni.javaee.ebay.bo;
 
+import java.util.Date;
+
 public class Enchere {
 
+	private Date dateEnchere;
+	private int montantEnchere;
+	private Utilisateur noUtilisateur;
+	private ArticleVendu noArticle;
+
 	public Enchere() {
-		// TODO Auto-generated constructor stub
+
+	}
+
+	/**
+	 * @param dateEnchere
+	 * @param montantEnchere
+	 * @param noUtilisateur
+	 * @param noArticle
+	 */
+	public Enchere(Date dateEnchere, int montantEnchere, Utilisateur noUtilisateur, ArticleVendu noArticle) {
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+		this.noUtilisateur = noUtilisateur;
+		this.noArticle = noArticle;
+	}
+
+	/**
+	 * @param dateEnchere
+	 * @param montantEnchere
+	 */
+	public Enchere(Date dateEnchere, int montantEnchere) {
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+
+	/**
+	 * @return the dateEnchere
+	 */
+	public Date getDateEnchere() {
+		return dateEnchere;
+	}
+
+	/**
+	 * @param dateEnchere
+	 *            the dateEnchere to set
+	 */
+	public void setDateEnchere(Date dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	/**
+	 * @return the montantEnchere
+	 */
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	/**
+	 * @param montantEnchere
+	 *            the montantEnchere to set
+	 */
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+	/**
+	 * @return the noUtilisateur
+	 */
+	public Utilisateur getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	/**
+	 * @param noUtilisateur
+	 *            the noUtilisateur to set
+	 */
+	public void setNoUtilisateur(Utilisateur noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	/**
+	 * @return the noArticle
+	 */
+	public ArticleVendu getNoArticle() {
+		return noArticle;
+	}
+
+	/**
+	 * @param noArticle
+	 *            the noArticle to set
+	 */
+	public void setNoArticle(ArticleVendu noArticle) {
+		this.noArticle = noArticle;
 	}
 
 }
