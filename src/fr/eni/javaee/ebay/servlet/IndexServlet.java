c4698404b4219c6b5e1652c8ee55765929bef868
mleclerc2019 <mleclerc2019@106-UC66-10.ad.campus-eni.fr>
2019-04-25 12:11:21 +0200

    Création et implémentation de IndexServlet

---
+++ src/fr/eni/javaee/ebay/servlet/IndexServlet.java
@@ -0,0 +1,54 @@
+package fr.eni.javaee.ebay.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.javaee.ebay.bll.EnchereManager;
+import fr.eni.javaee.ebay.bll.ManagerFactory;
+import fr.eni.javaee.ebay.bo.Enchere;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String PAGE_INDEX = "index.jsp";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * Cette méthode permet d'acceder à la page d'accueil du site
+	 * Retourne une liste d'enchère et une liste de catégorie à la jsp
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		EnchereManager enchereManager = ManagerFactory.getEnchereManager();		
+		
+		List<Enchere> listeEnchere = enchereManager.listerToutes();		
+		request.setAttribute("listeEnchere", listeEnchere);		
+		request.getRequestDispatcher( PAGE_INDEX ).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
