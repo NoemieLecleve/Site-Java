017842b047a53f2c6beb981cfe7091d100b988fd
mleclerc2019 <mleclerc2019@106-UC66-10.ad.campus-eni.fr>
2019-04-26 09:05:20 +0200

    Implémentation de l'affichage des articles à l'index du site

--- src/fr/eni/javaee/ebay/servlet/IndexServlet.java
+++ src/fr/eni/javaee/ebay/servlet/IndexServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.eni.javaee.ebay.bll.EnchereManager;
+import fr.eni.javaee.ebay.bll.ArticleManager;
 import fr.eni.javaee.ebay.bll.ManagerFactory;
 import fr.eni.javaee.ebay.bo.Enchere;
 
@@ -36,7 +36,7 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		EnchereManager enchereManager = ManagerFactory.getEnchereManager();		
+		ArticleManager enchereManager = ManagerFactory.getEnchereManager();		
 		
 		List<Enchere> listeEnchere = enchereManager.listerToutes();		
 		request.setAttribute("listeEnchere", listeEnchere);		
