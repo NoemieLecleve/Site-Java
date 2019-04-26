191c950a08de2a8a29df2f8a00110d7d4dc21c54
Isylda <noemie.lecleve@live.fr>
2019-04-26 09:14:49 +0200

    Ajout de la methode getArticleDAO

--- src/fr/eni/javaee/ebay/dal/DAOFactory.java
+++ src/fr/eni/javaee/ebay/dal/DAOFactory.java
@@ -1,11 +1,17 @@
 package fr.eni.javaee.ebay.dal;
 
+import fr.eni.javaee.ebay.dal.impl.ArticleDAOImpl;
 import fr.eni.javaee.ebay.dal.impl.UtilisateurDAOImpl;
 
 public class DAOFactory {
-	
+
 	public static UtilisateurDAO getUtilisateurDAO() throws DALException {
 		return new UtilisateurDAOImpl();
+	}
+
+	public static ArticleDAO getArticleDAO() throws DALException {
+		// Methode à revoir
+		return new ArticleDAOImpl();
 	}
 
 }
