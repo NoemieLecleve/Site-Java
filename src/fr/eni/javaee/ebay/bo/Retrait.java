47ac23191084d19b96845c806508501fdc7f290d
ltani2019 <ltani2019@106-UC66-11.ad.campus-eni.fr>
2019-04-30 10:36:55 +0200

    La jsp article vendu et la classe Retrait.  modification de la classe ArticleVendu

--- src/fr/eni/javaee/ebay/bo/Retrait.java
+++ src/fr/eni/javaee/ebay/bo/Retrait.java
@@ -2,8 +2,70 @@ package fr.eni.javaee.ebay.bo;
 
 public class Retrait {
 
+	private String rueRetrait;
+	
+	private String villeRetrait;
+	
+	private String codePostalRetrait;
+	
+	
+	
 	public Retrait() {
-		// TODO Auto-generated constructor stub
+		 
+	}
+
+	/**
+	 * @param rueRetrait
+	 * @param villeRetrait
+	 * @param codePostalRetrait
+	 */
+	public Retrait(String rueRetrait, String villeRetrait, String codePostalRetrait) {
+		 
+		this.rueRetrait = rueRetrait;
+		this.villeRetrait = villeRetrait;
+		this.codePostalRetrait = codePostalRetrait;
+	}
+
+	/**
+	 * @return the rueRetrait
+	 */
+	public String getRueRetrait() {
+		return rueRetrait;
+	}
+
+	/**
+	 * @param rueRetrait the rueRetrait to set
+	 */
+	public void setRueRetrait(String rueRetrait) {
+		this.rueRetrait = rueRetrait;
+	}
+
+	/**
+	 * @return the villeRetrait
+	 */
+	public String getVilleRetrait() {
+		return villeRetrait;
+	}
+
+	/**
+	 * @param villeRetrait the villeRetrait to set
+	 */
+	public void setVilleRetrait(String villeRetrait) {
+		this.villeRetrait = villeRetrait;
+	}
+
+	/**
+	 * @return the codePostalRetrait
+	 */
+	public String getCodePostalRetrait() {
+		return codePostalRetrait;
+	}
+
+	/**
+	 * @param codePostalRetrait the codePostalRetrait to set
+	 */
+	public void setCodePostalRetrait(String codePostalRetrait) {
+		this.codePostalRetrait = codePostalRetrait;
 	}
 
 }
